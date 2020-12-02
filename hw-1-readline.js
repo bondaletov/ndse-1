22e7c172dcb17c8035ac22f7d632f692c898a769
constantine <20419408+bondaletov@users.noreply.github.com>
2020-12-02 08:45:47 +0200

    Add files via upload

--- hw-1-readline.js
+++ hw-1-readline.js
@@ -1,22 +1,26 @@
 const readline = require('readline');
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+const rl = readline.createInterface(process.stdin);
 
 let countOfAttempts = 0;
-const secret = Math.floor(Math.random() * (+10 + 1 - +0)) + +0;
+let secret;
+const start = () => {
+    secret = Math.round(Math.random() * 100);
+    console.log('Загадано число в диапазоне от 0 до 100');
+};
+
+rl.on('resume', () => {
+    console.log('Readline resumed.');
+});
 
 rl.on('line', (line) => {
     countOfAttempts += 1;
     if (Number(line) === secret) {
-        rl.close();
+        console.log('\x1b[36m%s\x1b[0m', `Отгадано число ${secret} с попытки ${countOfAttempts}\n\n`);
+        start();
     } else {
         console.log(line > secret ? 'Больше' : 'Меньше');
     }
 });
 
-rl.on('close', () => {
-    console.log('\x1b[36m%s\x1b[0m', `Отгадано число ${secret} с попытки ${countOfAttempts}`);
-});
+start();
